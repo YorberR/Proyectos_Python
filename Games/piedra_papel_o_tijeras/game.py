6257e42acea5a35f0de78f5c979b1389fd5a53d8
YorberR <yorberrojas15@gmail.com>
2023-02-11 18:21:23 -0400

    Encapsulado el codigo en muchas funciones para ser reutilizadas y para que sea mas sencillo de leer y mejorada la mecanica del juego

--- Games/piedra_papel_o_tijeras/game.py
+++ Games/piedra_papel_o_tijeras/game.py
@@ -1,78 +1,76 @@
-import random 
+import random
+import os
+import time
 
-options = ('piedra', 'papel', 'tijera')
-rounds = 1
-computer_wins = 0 
+rounds = 0
+options = ["piedra" or "1", "papel", "tijera", "1", "2", "3"]
 user_wins = 0
+computer_wins = 0
 
-print("""
-      [ 🤖 Bienvenido al juego Piedra, Papel o tijera 🙋]
-                  >>> Ingresa una opción <<<
-      """)
-
-while True:
-    print('***' * 10)
-    print('Round ', rounds)
-    print('***' * 10)
-    
+def show_score(computer_wins, user_wins, rounds):
+    print('*' * 10)
+    print('Puntajes')
     print(f'''
-🤖 Computer wins: {computer_wins}
-🙋 User wins: {user_wins}
-          ''')
-    
-    user_option = input('>>> Piedra, papel o tijera => ').lower()
-    rounds += 1
-    
-    if not user_option in options:
-        print('Esa opción no es valida')
-        
-    computer_option = random.choice(options)
+    🤖 Computer wins: {computer_wins}
+    🙋 User wins: {user_wins}
+    ''')
+    print('*' * 10)
+    print(f''' Round #{rounds} ''')
 
-    print('User option => ', user_option)
-    print('Computer option => ', computer_option)
 
-    if user_option == computer_option:
-        print('Empate!\n')
-    elif user_option == 'piedra':
-        if computer_option == 'tijera':
-            print('🪨 Piedra gana a tijera ✂️')
-            print('¡User gana!\n')
-            user_wins += 1
-        else:
-            print('📄 Papel gana a piedra 🪨')
-            print('¡Computer gana!\n')
-            computer_wins += 1
-    elif user_option == 'papel':
-        if computer_option == 'piedra':
-            print('📄 Papel gana a piedra 🪨')
-            print('¡User gana!\n')
-            user_wins += 1
-        else:
-            print('✂️ Tijera gana a papel 📄')
-            print('¡Computer gana!\n')
-            computer_wins += 1
-    elif user_option == 'tijera':
-        if computer_option == 'papel':
-            print('✂️ Tijera gana a papel 📄')
-            print('¡User gana!\n')
-            user_wins += 1
-        else:
-            print('🪨 Piedra gana a tijera ✂️')
-            print('¡Computer gana!\n')
-            computer_wins += 1
-    
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "Empate!"
+    elif (user_choice == "piedra" or "1") and (computer_choice == "tijera" or "3"):
+        return "Ganaste!"
+    elif (user_choice == "papel" or "2") and (computer_choice == "piedra" or "1"):
+        return "Ganaste!"
+    elif (user_choice == "tijera" or "3") and (computer_choice == "papel"or "2"):
+        return "Ganaste!"
+    else:
+        return "Perdiste!"
+
+def show_round_result(user_choice, computer_choice):
+    result = determine_winner(user_choice, computer_choice)
+    if result == "Ganaste!":
+        global user_wins
+        user_wins += 1
+    elif result == "Perdiste!":
+        global computer_wins
+        computer_wins += 1
+    print('-' * 10)
+    print("Tu elección:", user_choice)
+    print("Elección de la computadora:", computer_choice)
+    print('-' * 10)
+    print(result)
+    print("Puntuación actual: Tú", user_wins, "Computadora", computer_wins)
+    return user_wins, computer_wins
+
+
+def determine_game_winner(user_wins, computer_wins):
     if computer_wins == 3:
         print(f'🎖️ El ganador es Computer con {computer_wins} puntos 🎖️')
-        break
-    
+        return 'Computer'
     if user_wins == 3:
         print(f'🎖️ El ganador es User con {user_wins} puntos 🎖️')
-        break
+        return 'User'
 
-        
-    # Cosas por hacer: 
-    # Creo un diccionario con las opciones y sus resultados.
-    # Crear una funcion para mostrar el puntaje de cada jugador.
-    # Crear una funcion para mostrar el resultado de cada ronda.
-    # Crear una funcion para determinar el ganador del juego.
-    # Eliminar los if anidados y se reemplazar por la funcion que determina el ganador.
+def run(rounds, options, user_wins, computer_wins):
+    while True:
+        rounds += 1
+        time.sleep(4)
+        os.system("clear")
+        show_score(computer_wins, user_wins, rounds)
+        print("Puedes escribir directamente tu opción o poner el número de tu opción")
+        user_choice = input("Elige piedra(1), papel(2) o tijera(3) puedes escribir tu: ").lower()
+        if user_choice in options:
+            computer_choice = random.choice(options)
+            user_wins, computer_wins = show_round_result(user_choice, computer_choice)
+            winner = determine_game_winner(user_wins, computer_wins)
+            if winner:
+                break
+        else:
+            print("Opción no válida, por favor elige piedra, papel o tijeras.")
+
+if __name__ == "__main__":
+    run(rounds, options, user_wins, computer_wins)
