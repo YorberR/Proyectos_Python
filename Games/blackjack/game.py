c4093a5ebccd6b70acbca95c5b9f7520c55d97fe
YorberR <yorberrojas15@gmail.com>
2023-03-04 19:58:57 -0400

    Idea de un juego para programar

---
+++ Games/blackjack/game.py
@@ -0,0 +1,23 @@
+#El Blackjack, es un juego de cartas, propio de los casinos con una
+#o más barajas inglesas de 52 cartas sin los comodines, que consiste
+#en sumar un valor lo más próximo a 21 pero sin pasarse. 
+#En un casino cada jugador de la mesa juega únicamente contra el crupier,
+#intentando conseguir una mejor jugada que este. El crupier está sujeto a
+#reglas fijas que le impiden tomar decisiones sobre el juego. Por ejemplo, 
+#está obligado a pedir carta siempre que su puntuación sume 16 o menos, y 
+#obligado a plantarse si suma 17 o más. Las cartas numéricas suman su valor,
+#las figuras suman 10 y el As vale 11 o 1, a elección del jugador. En el caso 
+#del crupier, los Ases valen 11 mientras no se pase de 21, y 1 en caso contrario.
+#La mejor jugada es conseguir 21 con solo dos cartas, esto es con un As más carta de valor 10.
+#Esta jugada se conoce como Blackjack o 21 natural. Un Blackjack gana sobre un 21 
+#conseguido con más de dos cartas. 
+
+import random
+import os
+
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
